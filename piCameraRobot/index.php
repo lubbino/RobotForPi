7cd4d9a944990e5e2d0f196275e374982af5dd5d
Ludvig Lif <mr.lubbin@gmail.com>
2024-10-24 21:04:38 +0000

    Changed title

--- piCameraRobot/index.php
+++ piCameraRobot/index.php
@@ -7,7 +7,7 @@ $server_ip = $_SERVER['SERVER_ADDR'];
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Control Motor and Track Mouse</title>
+    <title>Track Mouse</title>
     <style>
         body {
             font-family: Arial, sans-serif;
